1a2add3625275d462ae8d8e1988259e07c5dad47
mtdx <ms2asdk@gmail.com>
2018-02-13 07:05:17 -0700

    fix dopx 3

--- src/main/java/com/mma/mma/service/CsgoItemService.java
+++ src/main/java/com/mma/mma/service/CsgoItemService.java
@@ -95,7 +95,7 @@ public class CsgoItemService {
                 }
                 BigDecimal sp = item.getSp();
                 if (sp != null && sp.doubleValue() > 0) {
-                    if (sp.doubleValue() > 4 && (item.getDcx() < -30 || item.getDcx() > 30 || item.getDopx() > 50 || item.getDopx() < 5)) {
+                    if (sp.doubleValue() > 4 && (item.getDcx() < -30 || item.getDcx() > 30 || item.getDopx() > 35)) {
                         continue;
                     }
                     prices.put(item.getName(), sp.doubleValue());
